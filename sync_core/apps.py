5c1e8df2cf2647ddba9f5da8a2834b0d3cbb1339
avk <avk@it-solution.ru>
2026-01-10 01:08:51 +0300

    del apps

--- sync_core/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SyncCoreConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "utils.sync_core"
-    verbose_name = "Sync Core"
